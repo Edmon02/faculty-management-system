3d8edf3317589c9703c933807e73b122cc410091
Edmon02 <97591641+Edmon02@users.noreply.github.com>
2024-02-23 16:25:47 +0400

    Update server.py

--- server.py
+++ server.py
@@ -30,7 +30,6 @@ import pandas as pd
 from text_generation import Client
 import traceback
 from dotenv import load_dotenv
-from openai import OpenAI
 
 load_dotenv()
 
@@ -99,14 +98,11 @@ DATABASE =  os.path.join(cur_dir, 'pulpit.sqlite')
 API_URL = os.environ.get("API_URL", None)
 API_TOKEN = os.environ.get("API_TOKEN", None)
 # Set an environment variable for the key
-os.environ['OPENAI_API_KEY'] = os.environ.get("OPENAI_API_KEY", None)
-
-client_op = OpenAI()  # add api_key
 
 client = Client(API_URL, headers={"Authorization": f"Bearer {API_TOKEN}"})
 
 # Set secure configurations
-# app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Strict'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -132,45 +128,6 @@ app.config['SESSION_FILE_DIR'] = '/tmp/flask_session'
 # Set the lifetime of a permanent session
 app.permanent_session_lifetime = 60 * 60 * 12  # half day
 
-def convert_number_to_words(number: float) -> str:
-    p = inflect.engine()
-    words = p.number_to_words(number)
-    words = translator.translate(words, dest='hy').text
-    return words
-
-def process_text(text: str) -> str:
-    # Convert numbers to words
-    words = []
-    for word in text.split():
-        # Check if the word is a number
-        if re.search(r'\d', word):
-            words.append(convert_number_to_words(int(''.join(filter(str.isdigit, word)))))
-        else:
-            words.append(word)
-
-    # Join the words back into a sentence
-    processed_text = ' '.join(words)
-
-    # Remove URLs
-    processed_text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', processed_text)
-
-    return processed_text
-
-def tts(text, model, voice):
-    text = process_text(text)
-    response = client_op.audio.speech.create(
-        model=model,  # "tts-1","tts-1-hd"
-        voice=voice,  # 'alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'
-        input=text,
-    )
-
-    # Get the audio data as a NumPy array
-    audio_data = np.frombuffer(response.content, dtype=np.int16)
-
-    # Convert the audio data to bytes for storage in the database
-    audio_data_bytes = audio_data.tobytes()
-
-    return audio_data_bytes
 
 header = "A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions."
 prompt_template = "### Human: {query}\n### Assistant:{response}"
